ef60259f00559664a523ec732e42f9a1cf2a81b4
unknown <Benwu@localaway.com>
2020-02-19 21:01:12 -0800

    Fix ai/com site access modal UI size smaller

--- resources/views/frontend/access_code.blade.php
+++ resources/views/frontend/access_code.blade.php
@@ -1,19 +1,19 @@
 <div class="modal fade" id="ai_access_modal" role="dialog" aria-labelledby="aiModalLabel">
     <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
+        <div class="modal-content w-auto">
             <div class="modal-body p-5 mx-5">
             <form id="request-access-form" >
                 <div class="text-center">
-                    <img class="d-lg-block mx-auto my-5 w-100" src="/images/newlanding/logo.png" style="max-width:80px;" alt="">
+                    <img class="d-lg-block mx-auto my-4 w-100" src="/images/newlanding/logo.png" style="max-width:60px;" alt="">
                 </div>
                 <div class="col">
                     <div class="d-flex" style="flex-direction: column;height: 100%;justify-content: space-between;">
-                        <h2 class="font-weight-bold mb-4 question text-center congrate title font-orange">Welcome to Local Away</h2>
+                        <h3 class="font-weight-bold mb-4 question text-center congrate title font-orange">Welcome to Local Away</h3>
                         <h5 class="text-center mb-4 font-weight-bold">Enter your exculsive members <br> only access code to enter</h5>
                     </div>
                 </div>
                 <div class="form-group">
-                <input type="text" class="form-control" name="name" id="access-code" required placeholder="Access Code">
+                <input type="text" class="form-control h-auto" name="name" id="access-code" required placeholder="Access Code">
                     <span id="access_code_error" class="invalid-feedback" role="alert">
                         <strong>This access code does not match our record.</strong>
                     </span>
@@ -26,12 +26,12 @@
                 </div>
 
                 <div class="text-center mt-4">
-                    <input type="submit" id="btn-access-submit" class="btn btn-brown text-white mr-3 letter-spacing-1 text-center" value="Submit"/>
+                    <input type="submit" id="btn-access-submit" class="btn btn-brown text-white text-center btn-modal" value="Submit"/>
                 </div>
 
                 <div class="text-center mt-4">
                     <h5 class="text-center mb-4 font-weight-bold">Don't have a code?</h5>
-                    <input type="button" id="btn-request-access"  class="btn btn-brown text-white mr-3 letter-spacing-1 text-center" value="Request Access"/>
+                    <input type="button" id="btn-request-access"  class="btn btn-brown text-white text-center btn-modal" value="Request Access"/>
                 </div>
             </form>
             </div>
--- resources/views/frontend/request_access_code.blade.php
+++ resources/views/frontend/request_access_code.blade.php
@@ -1,24 +1,24 @@
 <div class="modal fade" id="ai_request_access_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
-    <div class="modal-content">
+    <div class="modal-content w-auto">
         <div class="modal-body p-5 mx-5">
         <form id="request_send_form" > <!--action='/survey' method='post'-->
         {{-- @csrf --}}
-        <img class="d-lg-block mx-auto my-5 w-100" src="/images/newlanding/logo.png" style="max-width:80px;" alt="">
+        <img class="d-lg-block mx-auto my-4 w-100" src="/images/newlanding/logo.png" style="max-width:60px;" alt="">
             <div class="col">
             <div class="d-flex" style="flex-direction: column;height: 100%;justify-content: space-between;">
-                <h2 class="font-weight-bold mb-4 question text-center congrate title font-orange">Welcome to Local Away</h2>
+                <h3 class="font-weight-bold mb-4 question text-center congrate title font-orange">Welcome to Local Away</h3>
                 <h5 class="text-center mb-4 font-weight-bold">We are excited to get to know you</h5>
             </div>
             </div>
             <div class="form-group">
-            <input type="text" class="form-control" name="name" id="name-text" required placeholder="Full Name">
+            <input type="text" class="form-control h-auto" name="name" id="name-text" required placeholder="Full Name">
             </div>
             <div class="form-group">
-            <input type="email" class="form-control" name="email" id="email-text" required placeholder="Email">
+            <input type="email" class="form-control h-auto" name="email" id="email-text" required placeholder="Email">
             </div>
             <div class="form-group">
-            <input type="text" class="form-control" name="phone" id="phone-text" placeholder="Phone(Optional)">
+            <input type="text" class="form-control h-auto" name="phone" id="phone-text" placeholder="Phone(Optional)">
             </div>
             <div class="form-group px-4">
             <label for="message-text" class="col-form-label">I am a..(Please select one)</label><br>
@@ -31,7 +31,7 @@
             </div>
 
             <div class="form-group">
-            <textarea type="text" class="form-control" name="note" id="note-text" placeholder="Note(Optional)" style="height: 8em;"></textarea>
+                <textarea type="text" class="form-control" name="note" id="note-text" placeholder="Note(Optional)"></textarea>
             </div>
             <!-- <div class="modal-footer"> -->
             <div class="clearfix text-center">
@@ -41,7 +41,7 @@
             </div>
 
             <div class="text-center mt-4">
-                <input type="submit" id="btn-request-send"  class="btn btn-brown text-white mr-3 letter-spacing-1 text-center" value="Request Access"/>
+                <input type="submit" id="btn-request-send"  class="btn btn-brown text-white text-center btn-modal" value="Request Access"/>
             </div>
             <!-- </div> -->
         </form>
