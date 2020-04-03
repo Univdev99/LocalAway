10ddf5275c9c631896519c8499221fdbf49c6c66
BinWu <benwu@localaway.com>
2020-04-03 22:46:17 +0800

    Fix font issue in localaway process section

--- resources/views/com/frontend/sections/work.blade.php
+++ resources/views/com/frontend/sections/work.blade.php
@@ -15,38 +15,38 @@
             </div>
             <div class="row mt-5">
               <div class="col-lg-4 col-md-12">
-                <p class='font-weight-bold mt-2 text-black text-center' style="height:60px;">
+                <p class='mt-2 text-black text-center font-avenir' style="height:60px;">
                   What is the fashion story you want to tell?
                 </p>
                   <div class = "d-flex text-center justify-content-center">
                     <h1 class = "font-weight-bold color-orange my-auto mx-0" style="color: #02BFAF;">1</h1>
                   </div>
                   <div  class = "text-center">
-                    <span class='font-avenir'>What is the occassion and what should your photos reflect? From work from home to hiking trails we can help you have fun.
+                    <span style="font-family: Helvetica !important;">What is the occassion and what should your photos reflect? From work from home to hiking trails we can help you have fun.
                     </span>
                   </div>
               </div>
               <div class="col-lg-4 col-md-12">
-                <p class='font-weight-bold mt-2 text-black text-center' style="height:60px;">
+                <p class='font-weight-bold mt-2 text-black text-center font-avenir' style="height:60px;">
                   It's all about you.
                 </p>
                   <div class = "d-flex text-center justify-content-center">
                     <h1 class = "font-weight-bold color-orange my-auto mx-0" style="color: #02BFAF;">2</h1>
                   </div>
                   <div  class = "text-center">
-                    <span class='font-avenir'>The more detail, the better! However if you prefer to chat with a stylist, we have that as an option too.
+                    <span style="font-family: Helvetica !important;">The more detail, the better! However if you prefer to chat with a stylist, we have that as an option too.
                     </span>
                   </div>
               </div>
               <div class="col-lg-4 col-md-12">
-                <p class='font-weight-bold mt-2 text-black text-center' style="height:60px;">
+                <p class='mt-2 text-black text-center font-avenir' style="height:60px;">
                   Sit back, relax, and enjoy your local fashion.
                 </p>
                   <div class = "d-flex text-center justify-content-center">
                     <h1 class = "font-weight-bold color-orange my-auto mx-0" style="color: #02BFAF;">3</h1>
                   </div>
                   <div  class = "text-center">
-                    <span class='font-avenir'>Our expert stylists will curate the perfect outfit recommendations for you, or you can browse hand-picked items. 
+                    <span style="font-family: Helvetica !important;">Our expert stylists will curate the perfect outfit recommendations for you, or you can browse hand-picked items. 
                     </span>
                   </div>
               </div>
