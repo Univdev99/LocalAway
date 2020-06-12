f2a8584a21596908531384df571e68546a85eb08
Bin Wu <benwu@localaway.com>
2020-06-11 17:44:07 -0700

    Fix stylist layout bug

--- resources/views/com/customer/section/shop.blade.php
+++ resources/views/com/customer/section/shop.blade.php
@@ -2,11 +2,11 @@
 
 @section('upcomingbox')
     <div class="col-12 col-md-12 d-block py-3 bg-green text-center m-auto">
-        <p class="text-white m-0">Your Style Capsule is on the way!</p>
+        <div class="text-white m-0">Your Style Capsule is on the way!</div>
     </div>
     <div class="row text-center m-auto">
         <form class="text-center form-shop" action="{{ route('customer.signup.payment') }}" method="get">
-            <h3> What's next?</h3>
+            <span class="h3 text-dark d-block"> What's next?</span>
             <p>Simply adjust any preferences or just add a note for your stylist.</p>
             <img src="/images/localaway-box.svg" width="300px">
             <div class="m-auto text-center">
